8de626159f4e7ca51ff2a0a64341c4b74327e5f0
AjdeDjokovic <623581439@qq.com>
2020-05-25 15:28:56 +0800

    code end

---
+++ code/cmake-build-debug/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/code/PL0_Compiler/code/lex.cpp" "D:/code/PL0_Compiler/code/cmake-build-debug/CMakeFiles/test.dir/lex.cpp.obj"
+  "D:/code/PL0_Compiler/code/main.cpp" "D:/code/PL0_Compiler/code/cmake-build-debug/CMakeFiles/test.dir/main.cpp.obj"
+  "D:/code/PL0_Compiler/code/parse.cpp" "D:/code/PL0_Compiler/code/cmake-build-debug/CMakeFiles/test.dir/parse.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
